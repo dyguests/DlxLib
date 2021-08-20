212cba1dfbe34d48d66a9fcf5bb75956cef0451d
fanhl <dyguests@qq.com>
2021-08-20 22:51:27 +0800

    Dlx.

---
+++ DlxLib/Dlx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DlxLib
+{
+    /// <summary>
+    /// Dancing links
+    /// </summary>
+    public class Dlx
+    {
+        public static int[][] Solve(int[,] matrix)
+        {
+            if (matrix == null)
+            {
+                throw new ArgumentNullException(nameof(matrix));
+            }
+
+            return new int[0][];
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("2333");
+        }
+    }
+}
