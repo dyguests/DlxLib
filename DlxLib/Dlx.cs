80329dd1b7865a373fd67192272c3b5f5cdacdd5
fanhl <dyguests@qq.com>
2021-08-21 20:38:09 +0800

    search logic.

--- DlxLib/Dlx.cs
+++ DlxLib/Dlx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DlxLib
 {
@@ -12,12 +13,74 @@ namespace DlxLib
             if (matrix == null) throw new ArgumentNullException(nameof(matrix));
 
             var h = BuildSparseMatrix(matrix);
-            return Solve(h);
+            var O = new List<DataObject>();
+            return Search(0, h, O);
         }
 
-        private static int[][] Solve(ColumnObject h)
+        /// <summary>
+        /// Our nondeterministic algorithm to find all exact covers can now be cast in the following explicit, deterministic form as a recursive procedure search(k), which is invoked initially with k = 0
+        /// </summary>
+        /// <param name="k"></param>
+        /// <param name="h"></param>
+        /// <param name="O"></param>
+        /// <returns></returns>
+        private static int[][] Search(int k, ColumnObject h, List<DataObject> O)
         {
-            throw new NotImplementedException();
+            // If R[h] = h, print the current solution (see below) and return.
+            if (h.R == h)
+            {
+                Console.WriteLine("result!?"); // todo
+                return null;
+            }
+
+            // Otherwise choose a column object c (see below).
+            var s = int.MaxValue;
+            var jj = h;
+            var c = jj;
+            while (jj != jj.R)
+            {
+                jj = jj.R as ColumnObject;
+
+                if (jj.S < s)
+                {
+                    c = jj;
+                    s = jj.S;
+                }
+            }
+
+
+            // Cover column c (see below).
+            CoverColumnC(c);
+
+
+            DataObject r = c;
+            while (r != r.D)
+            {
+                r = r.D; // 这行可以合到while中去？
+
+                O[k] = r;
+
+                var j = r;
+                while (j != r.R)
+                {
+                    // todo cover j
+                }
+
+                Search(k + 1, h, O);
+
+                r = O[k];
+                c = r.C;
+
+                j = r;
+                while (j != r.L)
+                {
+                    j = j.L;
+                    // todo un cover j
+                }
+            }
+
+            // todo un cover c
+            return null;
         }
 
         private static ColumnObject BuildSparseMatrix(int[,] matrix)
@@ -25,75 +88,111 @@ namespace DlxLib
             var h = new ColumnObject();
             for (var row = -1; row < matrix.GetLength(0); row++)
             {
-                DataObject c = h;
+                var c = h;
                 DataObject d = null;
                 for (var col = 0; col < matrix.GetLength(1); col++)
                 {
                     if (row == -1)
                     {
                         c.LinkRight(new ColumnObject());
-                        c = c.r;
+                        c = c.R as ColumnObject;
                     }
                     else
                     {
-                        c = (ColumnObject) c.r;
+                        c = c.R as ColumnObject;
                         if (matrix[col, row] == 1)
                         {
-                            c.u.LinkDown(new DataObject());
-                            // *1.将当列最下面的元素先暂存到c.u中(在跳舞前c.u还用不上),之后再还原。
-                            c.u = c.u.d;
-                            d?.LinkRight(c.u);
-                            d = c.u;
+                            c.U.LinkDown(new DataObject(c, row));
+                            // 这里更新c所在列有多少个DataObject
+                            c.S++;
+                            c.U = c.U.D;
+                            d?.LinkRight(c.U);
+                            d = c.U;
                         }
-                    }
-
-                    if (row == matrix.GetLength(0) - 1)
-                    {
-                        // 这里对*1进行还原
-                        c.u = c;
                     }
                 }
             }
 
             return h;
         }
+
+        private static void CoverColumnC(ColumnObject c)
+        {
+            c.R.L = c.L;
+            c.L.R = c.R;
+
+            DataObject i = c;
+            while (i != i.D)
+            {
+                i = i.D;
+
+                var j = i;
+                while (j != j.R)
+                {
+                    j = i.R;
+
+                    j.D.U = j.U;
+                    j.U.D = j.D;
+                    j.C.S--;
+                }
+            }
+        }
     }
 
     public class DataObject
     {
-        public DataObject l, r, u, d;
-        public ColumnObject c;
+        public DataObject L { get; set; }
+        public DataObject R { get; set; }
+        public DataObject U { get; set; }
+        public DataObject D { get; set; }
+        public ColumnObject C { get; set; }
 
-        public DataObject()
+        /// <summary>
+        /// 所在行
+        /// </summary>
+        public int Row { get; }
+
+        protected DataObject()
         {
-            l = r = u = d = this;
+            L = R = U = D = this;
+        }
+
+        public DataObject(ColumnObject c, int row) : this()
+        {
+            this.C = c;
+            this.Row = row;
         }
 
         public void LinkRight(DataObject another)
         {
-            this.r = another;
-            another.l = this;
+            this.R = another;
+            another.L = this;
+            another.R = this.L;
+            this.L.R = another;
         }
 
         public void LinkDown(DataObject another)
         {
-            this.d = another;
-            another.u = this;
+            this.D = another;
+            another.U = this;
         }
     }
 
     public class ColumnObject : DataObject
     {
-        public int s;
-
         /// <summary>
-        /// row index
+        /// size
         /// </summary>
-        public int n;
+        public int S { get; set; }
+
+        // /// <summary>
+        // /// name
+        // /// </summary>
+        // public int n;
 
         public ColumnObject()
         {
-            c = this;
+            C = this;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # DlxLib
 
+## Notices
+
+- 注意稀疏矩阵是一个十字环形链表。（“十字”、“环形”）
+
 ## Reference
 
 - [taylorjg/DlxLib](https://github.com/taylorjg/DlxLib)
