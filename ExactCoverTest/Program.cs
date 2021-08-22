25dee8047c66fb3a45d2e9ac466dcf429e61aed8
fanhl <dyguests@qq.com>
2021-08-22 11:00:58 +0800

    add test util.

--- ExactCoverTest/Program.cs
+++ ExactCoverTest/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Linq;
 using DlxLib;
+using TestUtil;
 
 namespace ExactCoverTest
 {
@@ -68,14 +68,6 @@ namespace ExactCoverTest
             }
 
             Console.WriteLine("-------- end --------");
-        }
-
-        private static string MatrixToString(this int[,] matrix)
-        {
-            return string.Join("\n", matrix.OfType<int>()
-                .Select((value, index) => new {value, index})
-                .GroupBy(x => x.index / matrix.GetLength(1))
-                .Select(x => $"{{{string.Join(",", x.Select(y => y.value))}}}"));
         }
     }
 }
---
+++ TestUtil/Util.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace TestUtil
+{
+    public static class Util
+    {
+        public static string MatrixToString(this int[,] matrix)
+        {
+            return string.Join("\n", matrix.OfType<int>()
+                .Select((value, index) => new {value, index})
+                .GroupBy(x => x.index / matrix.GetLength(1))
+                .Select(x => $"{{{string.Join(",", x.Select(y => y.value))}}}"));
+        }
+    }
+}
