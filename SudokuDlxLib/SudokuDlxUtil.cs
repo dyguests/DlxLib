2af9cc4389f41b7879ba2560983c62b7e225143f
fanhl <dyguests@qq.com>
2021-08-23 23:10:43 +0800

    add SudokuDlxUtil.

---
+++ SudokuDlxLib/SudokuDlxUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace SudokuDlxLib
+{
+    public static class SudokuDlxUtil
+    {
+        public static int[,] SudokuToMatrix(Sudoku.Sudoku sudoku)
+        {
+            return new int[0, 0];
+        }
+    }
+}
