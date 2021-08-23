94ebb28c038cdc8ce3c8d46ce64ece3d547d2ad0
fanhl <dyguests@qq.com>
2021-08-23 23:07:10 +0800

    sudoku prj.

---
+++ Sudoku/Sudoku.cs
@@ -0,0 +1,12 @@
+namespace Sudoku
+{
+    public class Sudoku
+    {
+        /// <summary>
+        /// 1~9 or empty(0)
+        /// </summary>
+        public int[] initNumbers;
+
+        public int[] solutionNumbers;
+    }
+}
