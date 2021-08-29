e946bcf442810b8a9f0fbf614a0e747355c5eafa
fanhl <dyguests@qq.com>
2021-08-30 00:07:45 +0800

    works.

--- SudokuTest/Tests.cs
+++ SudokuTest/Tests.cs
@@ -279,5 +279,85 @@ namespace SudokuTest
 
             Assert.True(solutions.Length == 1);
         }
+
+        [Test]
+        public void TestKillerSudokusDifficulty2()
+        {
+            var sudoku = new Sudoku
+            {
+                initNumbers = new[]
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0,
+                },
+                rules = new Rule[]
+                {
+                    new NormalRule(),
+                    new CageRule
+                    {
+                        cages = new[]
+                        {
+                            new CageRule.Cage {sum = 21, indexes = new[] {72, 73, 74,}},
+                            new CageRule.Cage {sum = 14, indexes = new[] {75, 66,}},
+                            new CageRule.Cage {sum = 9, indexes = new[] {76, 77,}},
+                            new CageRule.Cage {sum = 7, indexes = new[] {78, 69,}},
+                            new CageRule.Cage {sum = 11, indexes = new[] {79, 70,}},
+                            new CageRule.Cage {sum = 10, indexes = new[] {80, 71,}},
+                            new CageRule.Cage {sum = 8, indexes = new[] {63, 54,}},
+                            new CageRule.Cage {sum = 15, indexes = new[] {64, 65, 56,}},
+                            new CageRule.Cage {sum = 10, indexes = new[] {55, 46, 37,}},
+                            new CageRule.Cage {sum = 15, indexes = new[] {67, 58, 57,}},
+                            new CageRule.Cage {sum = 7, indexes = new[] {68, 59,}},
+                            new CageRule.Cage {sum = 16, indexes = new[] {61, 60, 51,}},
+                            new CageRule.Cage {sum = 7, indexes = new[] {62,}},
+                            new CageRule.Cage {sum = 12, indexes = new[] {53, 52, 43,}},
+                            new CageRule.Cage {sum = 7, indexes = new[] {45, 36,}},
+                            new CageRule.Cage {sum = 12, indexes = new[] {47, 48,}},
+                            new CageRule.Cage {sum = 16, indexes = new[] {38, 39,}},
+                            new CageRule.Cage {sum = 1, indexes = new[] {50,}},
+                            new CageRule.Cage {sum = 22, indexes = new[] {49, 40, 41, 42,}},
+                            new CageRule.Cage {sum = 12, indexes = new[] {27, 28, 29,}},
+                            new CageRule.Cage {sum = 19, indexes = new[] {30, 31, 32,}},
+                            new CageRule.Cage {sum = 28, indexes = new[] {25, 34, 33, 24, 15,}},
+                            new CageRule.Cage {sum = 15, indexes = new[] {44, 35, 26,}},
+                            new CageRule.Cage {sum = 13, indexes = new[] {18, 19, 20,}},
+                            new CageRule.Cage {sum = 8, indexes = new[] {9, 0,}},
+                            new CageRule.Cage {sum = 9, indexes = new[] {10,}},
+                            new CageRule.Cage {sum = 9, indexes = new[] {1, 2,}},
+                            new CageRule.Cage {sum = 12, indexes = new[] {11, 12, 21, 3,}},
+                            new CageRule.Cage {sum = 12, indexes = new[] {22, 23,}},
+                            new CageRule.Cage {sum = 10, indexes = new[] {13, 4,}},
+                            new CageRule.Cage {sum = 20, indexes = new[] {14, 5, 6, 7,}},
+                            new CageRule.Cage {sum = 18, indexes = new[] {16, 17, 8,}},
+                        },
+                    },
+                }
+            };
+            Console.WriteLine("Sudoku:\n" + sudoku.initNumbers.NumbersToString());
+
+            var matrix = SudokuDlxUtil.SudokuToMatrix(sudoku);
+            MatrixUtil.PrintMatrix(matrix);
+            var solutions = Dlx.Solve(matrix.matrix, matrix.primaryColumns, matrix.secondaryColumns).ToArray();
+            Console.WriteLine("Solution: count:" + solutions.Length + "\n");
+            foreach (var result in solutions)
+            {
+                Console.WriteLine(String.Join(",", result));
+            }
+
+            foreach (var solution in solutions)
+            {
+                var solutionNumbers = SudokuDlxUtil.SolutionToNumbers(sudoku, matrix.matrix, solution);
+                Console.WriteLine("Sudoku Solution:\n" + solutionNumbers.NumbersToString());
+            }
+
+            Assert.True(solutions.Length == 1);
+        }
     }
 }
