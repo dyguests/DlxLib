4b9f073c7b1b18f72ed7a613265138f61e303b54
fanhl <dyguests@qq.com>
2021-09-30 00:04:09 +0800

    generate and convert Diagonal 2.

--- SudokuConverterTest/DiagonalTests.cs
+++ SudokuConverterTest/DiagonalTests.cs
@@ -14,7 +14,58 @@ namespace SudokuConverterTest
             var count = 10;
             for (int i = 0; i < count; i++)
             {
-                var sudoku = SudokuGenerator.GenerateDiagonalSudoku(50);
+                var sudoku = SudokuGenerator.GenerateDiagonalSudoku(30,10);
+                Console.WriteLine(sudoku.ToDataString());
+            }
+
+            Assert.True(true);
+        }
+
+        [Test]
+        public void TestGenerateDiagonalSudoku2()
+        {
+            var count = 15;
+            for (int i = 0; i < count; i++)
+            {
+                var sudoku = SudokuGenerator.GenerateDiagonalSudoku(40, 10);
+                Console.WriteLine(sudoku.ToDataString());
+            }
+
+            Assert.True(true);
+        }
+
+        [Test]
+        public void TestGenerateDiagonalSudoku3()
+        {
+            var count = 15;
+            for (int i = 0; i < count; i++)
+            {
+                var sudoku = SudokuGenerator.GenerateDiagonalSudoku(30, 25);
+                Console.WriteLine(sudoku.ToDataString());
+            }
+
+            Assert.True(true);
+        }
+
+        [Test]
+        public void TestGenerateDiagonalSudoku4()
+        {
+            var count = 10;
+            for (int i = 0; i < count; i++)
+            {
+                var sudoku = SudokuGenerator.GenerateDiagonalSudoku(30, 35);
+                Console.WriteLine(sudoku.ToDataString());
+            }
+
+            Assert.True(true);
+        }
+        [Test]
+        public void TestGenerateDiagonalSudoku5()
+        {
+            var count = 10;
+            for (int i = 0; i < count; i++)
+            {
+                var sudoku = SudokuGenerator.GenerateDiagonalSudoku(25, 50);
                 Console.WriteLine(sudoku.ToDataString());
             }
 
