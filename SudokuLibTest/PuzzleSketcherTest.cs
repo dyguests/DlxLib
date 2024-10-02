328cf36dc0438bd9f9a18cdc6a9825e28546163a
fanhl <dyguests@qq.com>
2024-10-03 00:26:42 +0800

    SudokuDlxUtil

--- SudokuLibTest/PuzzleSketcherTest.cs
+++ SudokuLibTest/PuzzleSketcherTest.cs
@@ -13,7 +13,7 @@ namespace SudokuLibTest
         [Test]
         public void Test1()
         {
-            var sketch = "...........91......1..5.36..9..82..7.4..35..2..27..63..5.....4...1...9.....421...";
+            const string sketch = "...........91......1..5.36..9..82..7.4..35..2..27..63..5.....4...1...9.....421...";
             var puzzle = PuzzleSketcher.FromSketch(sketch);
             Console.WriteLine($"sketch:{sketch}");
             var formattedDigits = puzzle.Digits
