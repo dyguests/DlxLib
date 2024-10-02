668af0e94c282ecd02f0e388c1e71e55477b2c91
fanhl <dyguests@qq.com>
2024-10-03 00:08:44 +0800

    IPuzzle

---
+++ SudokuLib/Puzzle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SudokuLib
+{
+    public interface IPuzzle
+    {
+        int[] Digits { get; }
+        int[] Solution { get; }
+    }
+
+    public class Puzzle : IPuzzle
+    {
+        public int[] Digits { get; }
+        public int[] Solution { get; }
+
+        public Puzzle(int[] digits)
+        {
+            if (digits == null || digits.Length == 0)
+            {
+                throw new ArgumentException("Digits must be initialized", nameof(digits));
+            }
+
+            Digits = digits;
+            Solution = new int[digits.Length];
+        }
+    }
+}
