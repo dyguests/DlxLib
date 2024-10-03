9cfe376eee52b6b07fd450ad121f8885fb6e90cd
fanhl <dyguests@qq.com>
2024-10-03 23:15:11 +0800

    sketch -> Puzzle

--- SudokuDlxLibTest/SudokuDlxUtilTest.cs
+++ SudokuDlxLibTest/SudokuDlxUtilTest.cs
@@ -13,7 +13,7 @@ namespace SudokuDlxLibTest
         public void Setup() { }
 
         [Test]
-        public void Test1()
+        public void TestPuzzleOnly()
         {
             const string sketch = "...........91......1..5.36..9..82..7.4..35..2..27..63..5.....4...1...9.....421...";
             var puzzle = PuzzleSketcher.FromSketch(sketch);
@@ -29,7 +29,28 @@ namespace SudokuDlxLibTest
                 puzzle.SetSolution(solution);
                 Console.WriteLine($"sudoku Solution:\n{solution.DigitsToDisplay()}");
             }
-            var sketch2 = PuzzleSketcher.ToSketch(puzzle);
+            var sketch2 = PuzzleSketcher.ToSketch(puzzle, useMask: false);
+            Console.WriteLine($"solution sketch:\n{sketch2}");
+            Assert.Pass();
+        }
+        [Test]
+        public void TestPuzzleWithSolution()
+        {
+            const string sketch = "32586719486914327571425936819368245764793581258271463925839674143157892697642158c";
+            var puzzle = PuzzleSketcher.FromSketch(sketch);
+            Console.WriteLine($"sketch:\n{sketch}");
+            Console.WriteLine($"puzzle:\n{puzzle.ToDisplay()}");
+            var matrix = SudokuDlxUtil.ToMatrix(puzzle);
+            // Console.WriteLine($"matrix:\n{matrix.ToDisplay()}");
+            var dlx = new Dlx(matrix);
+            foreach (var result in dlx.Solve())
+            {
+                Console.WriteLine("dlx Solution:" + string.Join(",", result));
+                var solution = SudokuDlxUtil.ToSolution(puzzle, matrix, result);
+                puzzle.SetSolution(solution);
+                Console.WriteLine($"sudoku Solution:\n{solution.DigitsToDisplay()}");
+            }
+            var sketch2 = PuzzleSketcher.ToSketch(puzzle, useMask: false);
             Console.WriteLine($"solution sketch:\n{sketch2}");
             Assert.Pass();
         }
--- SudokuLib/PuzzleSketcher.cs
+++ SudokuLib/PuzzleSketcher.cs
@@ -9,12 +9,16 @@ namespace SudokuLib
         private const char CharDigit1 = '1';
         private const char CharDigit9 = '9';
         private const char CharMask1 = 'a';
+        private const char CharMask9 = 'i';
 
         public static IPuzzle FromSketch(string sketch)
         {
             var lines = sketch.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).Let(strings => new Queue<string>(strings));
 
-            var digits = lines.Let(queue =>
+            var digits = new int[9 * 9];
+            var solution = new int[9 * 9];
+
+            var digitsSketch = lines.Let(queue =>
             {
                 // 不断从队列中 Dequeue，直到找到非空字符串为止
                 while (queue.Count > 0)
@@ -26,18 +30,40 @@ namespace SudokuLib
 
                 // 如果队列中全是空，返回 null
                 return null;
-            })?.Select(c => c is >= CharDigit1 and <= CharDigit9 ? c + 1 - CharDigit1 : 0).ToArray() ?? throw new Exception("sketch is empty");
+            }) ?? throw new Exception("sketch is empty");
+            for (var i = 0; i < digitsSketch.Length; i++)
+            {
+                var c = digitsSketch[i];
+                if (c is >= CharDigit1 and <= CharDigit9)
+                {
+                    digits[i] = c - CharDigit1 + 1;
+                    solution[i] = c - CharDigit1 + 1;
+                }
+                else if (c is >= CharMask1 and <= CharMask9)
+                {
+                    solution[i] = c - CharMask1 + 1;
+                }
+            }
 
             return new Puzzle(digits);
         }
 
-
-        public static string ToSketch(IPuzzle puzzle, bool showSolution = true)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="puzzle"></param>
+        /// <param name="showSolution"></param>
+        /// <param name="useMask"></param>
+        /// <returns>是否加一层蒙层；是否区分迷题和迷底；是否区分digits/solution</returns>
+        public static string ToSketch(IPuzzle puzzle, bool showSolution = true, bool useMask = true)
         {
             var enumerable = Enumerable.Range(0, puzzle.Digits.Length).Select(i =>
             {
                 if (puzzle.Digits[i] != 0) return (char)(CharDigit1 + (puzzle.Digits[i] - 1));
-                if (showSolution && puzzle.Solution[i] != 0) return (char)(CharMask1 + (puzzle.Solution[i] - 1));
+                if (showSolution && puzzle.Solution[i] != 0)
+                {
+                    return (char)(useMask ? CharMask1 : CharDigit1 + (puzzle.Solution[i] - 1));
+                }
                 return '.';
             });
             var digitsSketch = string.Join("", enumerable);
