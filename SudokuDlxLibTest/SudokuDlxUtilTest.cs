3531b02d2eea2bf1c9163954753a9cca2c6c854c
fanhl <dyguests@qq.com>
2024-10-03 10:37:43 +0800

    SudokuDlxUtilTest

---
+++ SudokuDlxLibTest/SudokuDlxUtilTest.cs
@@ -0,0 +1,23 @@
+using System;
+using NUnit.Framework;
+using SudokuLib;
+using SudokuLibTest;
+
+namespace SudokuDlxLibTest
+{
+    public class SudokuDlxUtilTest
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void Test1()
+        {
+            const string sketch = "...........91......1..5.36..9..82..7.4..35..2..27..63..5.....4...1...9.....421...";
+            var puzzle = PuzzleSketcher.FromSketch(sketch);
+            Console.WriteLine($"sketch:{sketch}");
+            Console.WriteLine($"puzzle:\n{puzzle.ToDisplay()}");
+            Assert.Pass();
+        }
+    }
+}
--- SudokuLibTest/PuzzleTestUtil.cs
+++ SudokuLibTest/PuzzleTestUtil.cs
@@ -11,7 +11,7 @@ namespace SudokuLibTest
             return puzzle.Digits
                 .Select((digit, index) => new { digit, index })
                 .GroupBy(x => x.index / 9, x => x.digit)
-                .Select(group => string.Join("", group))
+                .Select(group => string.Join(" ", group))
                 .Aggregate((current, next) => current + Environment.NewLine + next);
         }
     }
