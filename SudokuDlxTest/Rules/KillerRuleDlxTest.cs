5afae7360cb1ff2b195c8d38dffc05cbc8a430ee
dygue <dyguests@qq.com>
2024-10-25 20:16:30 +0800

    还有点问题

--- SudokuDlxTest/Rules/KillerRuleDlxTest.cs
+++ SudokuDlxTest/Rules/KillerRuleDlxTest.cs
@@ -28,6 +28,22 @@ namespace SudokuDlxLibTest.Rules
         {
             var permutations = KillerRuleDlx.TestGetPossiblePermutations(
                 new[] { 1, 2, 3 },
+                new[] { new[] { 2, 3 }, new[] { 1, 2, 3 }, new[] { 1, 2, 3 } }
+            ).ToArray();
+
+            foreach (var permutation in permutations)
+            {
+                Console.WriteLine(string.Join(",", permutation));
+            }
+
+            Assert.That(permutations.Length, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Test_GetPossiblePermutations3()
+        {
+            var permutations = KillerRuleDlx.TestGetPossiblePermutations(
+                new[] { 1, 2, 3 },
                 new[] { new[] { 1, 2, }, new[] { 1, 2, 3 }, new[] { 1, 2, 3 } }
             ).ToArray();
 
