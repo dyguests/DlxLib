b64224065173c47aa1536e56bd0fb4c1fa3a9f3c
dyguests <dyguests@qq.com>
2025-02-05 17:03:50 +0800

    _items

--- SudokuLib/Rules/KropkiRule.cs
+++ SudokuLib/Rules/KropkiRule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace SudokuLib.Rules
 {
@@ -15,16 +16,32 @@ namespace SudokuLib.Rules
             if (string.IsNullOrWhiteSpace(sketch)) return null;
             if (!sketch.StartsWith(RulePrefix)) return null;
             var contentSketch = sketch.Substring(RulePrefix.Length).Trim();
-            contentSketch.Split(KropkiSeparator)
+            var kropkis = contentSketch.Split(KropkiSeparator)
                 .Select(item => item.Trim())
                 .SkipWhile(string.IsNullOrEmpty)
-                ;
-            return null;
+                .Select(aSketch => Consecutive.Instance.FromSketch(aSketch) ?? Ratio.Instance.FromSketch(aSketch))
+                .Where(item => item != null)
+                .Select(rule => rule as Kropki)
+                .Select(rule => rule!)
+                .ToArray();
+            return new KropkiRule(kropkis);
         }
 
         public override string ToSketch()
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder();
+            sb.Append(RulePrefix).Append(" ");
+            for (var i = 0; i < _items.Length; i++)
+            {
+                var cage = _items[i];
+                sb.Append(cage.ToSketch());
+                if (i < _items.Length - 1)
+                {
+                    sb.Append(KropkiSeparator);
+                }
+            }
+
+            return sb.ToString();
         }
 
         #endregion
@@ -34,15 +51,51 @@ namespace SudokuLib.Rules
         private const string RulePrefix = "Kropki";
         private const string KropkiSeparator = ";";
 
+        private readonly Kropki[] _items;
+        public Kropki[] ReadonlyItems => (Kropki[])_items.Clone();
+
+        private KropkiRule(params Kropki[] items)
+        {
+            _items = items;
+        }
+
+        public abstract class Kropki : Rule { }
+
         /// <summary>
         /// 白点约束：表示相邻格子的数字是连续的
         /// </summary>
-        public class Consecutive { }
+        public class Consecutive : Kropki
+        {
+            public static Consecutive Instance { get; } = new Consecutive();
+
+            public override IRule? FromSketch(string sketch)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string ToSketch()
+            {
+                throw new NotImplementedException();
+            }
+        }
 
         /// <summary>
         /// 黑点约束：表示相邻格子的数字存在倍数关系
         /// </summary>
-        public class Ratio { }
+        public class Ratio : Kropki
+        {
+            public static Ratio Instance { get; } = new Ratio();
+
+            public override IRule? FromSketch(string sketch)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string ToSketch()
+            {
+                throw new NotImplementedException();
+            }
+        }
 
         #endregion
     }
